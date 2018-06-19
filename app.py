50be8a2e24ddb74bcb71703b9d6994ec05ec2786
adsamaz <33390767+adsamaz@users.noreply.github.com>
2018-06-19 11:24:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,9 @@ fridayDone = False
 
 while True:
     now = datetime.datetime.now()
-    #print("HEJ")
-    if now.weekday() == 1 and now.strftime("%H:%M") == "11:19" and not wednesdayDone:  # On wednesday 09:00, print the current fikaperson
+    print("HEJ")
+    if now.weekday() == 1 and now.strftime("%H:%M") == "11:25" and not wednesdayDone:  # On wednesday 09:00, print the current fikaperson
+        print("Monday message sent: " + request.text)
         file = open("namnlista", "r", encoding="utf-8-sig")
         names = file.readlines()
         file.close()
